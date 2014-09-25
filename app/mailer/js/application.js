1323f9318012b189f092c136745ea063d48e48e8
Dooley <nsewolo@gmail.com>
2014-09-24 23:41:43 -0400

    required files added

---
+++ app/mailer/js/application.js
@@ -0,0 +1,4 @@
+//= require_tree ../node_modules
+//= require_tree ../lib
+//= require ./mailer
+//= require_tree .
---
+++ app/mailer/js/routes.js
@@ -0,0 +1,6 @@
+var App = require('./mailer');
+
+App.Router.map(function () {
+
+});
+
